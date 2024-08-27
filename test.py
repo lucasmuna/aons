4fdd96fc5ca62bac66eebcf3636d84fbfe6409ad
Lucas Munaretto <lucas.munaretto@hotmail.com>
2024-08-27 22:12:53 +0200

    Fix _Key and child parsers and add get and set item to it

--- test.py
+++ test.py
@@ -7,10 +7,20 @@ aons_data_file = pathlib.Path(__file__).parent / "data.aons"
 aons_schema_file = pathlib.Path(__file__).parent / "schema.aons"
 
 aons_data = aons.AonsData(aons_data_file)
-aons_schema = aons.AonsSchema(aons_data_file)
+aons_schema = aons.AonsSchema(aons_schema_file)
 
-print(aons_data.get_dict())
-aons_data.get_dict_with_comments()
+# print
+(aons_data.get_dict())
+(aons_data.get_dict_with_comments())
 
-aons_schema.get_dict()
-aons_schema.get_dict_with_comments()
+# print
+(aons_schema.get_dict())
+(aons_schema.get_dict_with_comments())
+
+print(aons_data["my_list_int"][0])
+aons_data["my_list_int"][0] = 1
+print(aons_data["my_list_int"][0])
+
+# aons.load(aons_data_file)
+
+# aons.validate(aons_data, aons_schema)
