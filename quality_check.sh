f82aec9dc2dc6468d3b3e17fc86626122c328323
Lucas Munaretto <lucas.munaretto@hotmail.com>
2024-12-06 17:36:44 +0100

    Call poetry install with `quality_check.sh`

--- quality_check.sh
+++ quality_check.sh
@@ -1,9 +1,28 @@
 # We could add a poetry install here
+BANNER=------------------------------------------------------------------
 
+echo poetry
+echo $BANNER
+poetry install --with dev
+echo
+echo pylint
+echo $BANNER
 poetry run pylint aons.py
+echo
+echo isort
+echo $BANNER
 poetry run isort aons.py
+echo
+echo black
+echo $BANNER
 poetry run black aons.py
+echo
+echo ruff
+echo $BANNER
 poetry run ruff check aons.py
+echo
+echo mypy
+echo $BANNER
 poetry run mypy aons.py
 
 # We need tests :)
